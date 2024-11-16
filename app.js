15da6a2743cbcf6e859acff3d08c04c58f71d3b4
kadir-zeren <k4dzeren@gmail.com>
2024-11-16 20:14:50 +0300

    createImage

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 //* ------ Selectors ------ */
+
+//? Resimler
+const rockImg = document.getElementById("rock");
+const paperImg = document.getElementById("paper");
+const scissorImg = document.getElementById("scissor");
+
+//? Secilen elemanlarin tayicilari
+const yourChoiceDiv = document.getElementById("your-choice");
+
 //* ------ Variables ------ */
+let image = document.createElement("img");
+
 //* ------ Event Listeners ------ */
+rockImg.addEventListener("click", () => {
+  image.src = "./assets/rock.png";
+  image.alt = "rock";
+  yourChoiceDiv.appendChild(image);
+
+  //? innerHTML
+  // yourChoiceDiv.innerHTML = `<img src="./assets/rock.png" alt="rock">`;
+});
+
 //* ------ Functions ------ */
